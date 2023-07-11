4a70084ff4a0728afba5fec941df33b4f839d725
alepacox <pacini.alessandro1@gmail.com>
2023-07-11 15:11:43 +0200

    Responsive tweaks

--- static/frontend/js/common.js
+++ static/frontend/js/common.js
@@ -24,9 +24,20 @@ $(document).ready(function($) {
 	$(window).resize(function() {
 		var window_height = ($(window).height() > 700) ? $(window).height() : 700;
 		
-		$('.colour').css('height', Math.floor(window_height/menu_items_number) );
-		$('.colour').last().css('height', Math.floor(window_height/menu_items_number) + window_height%menu_items_number);
-		$('.colours').css({'height': window_height});
+		$(window).on("resize", function(){
+			if($(window).width() < 990){
+				$('.logo').attr("src","https://elearning.santannapisa.it/pluginfile.php/1/theme_adaptable/adaptablemarkettingimages/0/logo-compatto-rosso.png");
+				$('.colour').css('height', Math.floor((window_height/2)/menu_items_number) );
+				$('.colours').css({'height': window_height/2});
+			}
+			else{
+				$('.logo').attr("src","https://elearning.santannapisa.it/pluginfile.php/1/theme_adaptable/logo/1661430207/logo2x.png");
+				$('.colour').css('height', Math.floor(window_height/menu_items_number) );
+				$('.colours').css({'height': window_height});
+			}
+		})
+		  
+
 		$('.circle, .home-block').height($('.colours').height());
 		
 		$('.circle.png img').height(window_height);
